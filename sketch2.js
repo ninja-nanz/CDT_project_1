5b1d46978fd8484336460ef01ee156527dab54be
Stefania La Vattiata Sanchez <stefanialvs@gmail.com>
2020-09-22 21:13:37 -0400

    voice recording added

--- sketch2.js
+++ sketch2.js
@@ -1,7 +1,13 @@
 let headerFont, myFont1, myFont2, myFont3, myFont4;
-var input;
+let timer = 5
 let button;
 let names = [];
+let buttonRecord;
+let buttonPlay;
+let buttonStop;
+let col1, col2, col3;
+let mic, recorder, soundFile;
+var input;
 function preload() {
   headerFont = loadFont('/resources/BebasNeue-Regular.ttf');
   myFont1 = loadFont('/resources/CircularStd-Book.otf');
@@ -13,12 +19,56 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(59, 59, 59);
+  col1 = color (196, 0, 0);
+  col2 = color (60,179,113);
+  col3 = color (235, 192, 52);
   input = createInput();
-  input.position(50, 300);
+  input.position(50, 220);
   button = createButton('Submit');
-  button.position(50, 330);
+  button.position(50, 250);
+  button.style('background-color', col3);
   button.mousePressed(newName);
+  buttonRecord = createButton('Record');
+  buttonRecord.style('background-color', col1);
+  buttonRecord.position(50, 80);
+  buttonRecord.mousePressed(recordAudio);
+  buttonStop = createButton('Stop');
+  buttonStop.style('background-color', col1);
+  buttonStop.position(120, 80);
+  buttonStop.mousePressed(stopRecording);
+  buttonPlay = createButton('Play');
+  buttonPlay.style('background-color', col2);
+  buttonPlay.position(50, 110);
+  buttonPlay.mousePressed(playAudio);
+
+  // create an audio in
+  mic = new p5.AudioIn();
+
+  // users must manually enable their browser microphone for recording to work properly!
+  mic.start();
+    
+  // create a sound recorder
+  recorder = new p5.SoundRecorder();
+    
+  // connect the mic to the recorder
+  recorder.setInput(mic);
+    
+  // create an empty sound file that we will use to playback the recording
+  soundFile = new p5.SoundFile();
 }
+
+function recordAudio() {
+  recorder.record(soundFile);
+}
+
+function stopRecording() {
+  recorder.stop();
+}
+
+function playAudio() {
+  soundFile.play();
+}
+
 
 function newName() {
     fill(255,255,255)
@@ -29,7 +79,7 @@ function newName() {
   class Name {
     constructor(name) {
       this.x = random(width);
-      this.y = random(400, height);
+      this.y = random(300, height);
       this.speed = random(.5, 2);
       this.name = name;
       this.font = random([myFont1,myFont3,myFont4]);
@@ -47,7 +97,7 @@ function newName() {
     }
   
     display() {
-      fill(235, 192, 52);
+      fill(255, 255, 255, 80);
       textFont(this.font);
       textSize(30);
       text(this.name, this.x, this.y);
@@ -59,9 +109,9 @@ function draw() {
     textFont(headerFont);
     textSize(70)
     fill(235, 192, 52);
-    text('Black Lives Matter', windowWidth/2, 100);
+    text('Black Lives Matter', (windowWidth/2)-150, 100);
     textSize(30)
-    text("Enter your name", 50, 230);
+    text("Enter your name", 50, 200);
     for (let i = 0; i < names.length; i++) {
       names[i].move();
       names[i].display();
