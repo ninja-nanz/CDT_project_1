6e6af7002b9019875488fe94df55840a046cf23a
Stefania La Vattiata Sanchez <stefanialvs@gmail.com>
2020-10-04 21:37:35 -0400

    soundwave added

--- sketch.js
+++ sketch.js
@@ -32,6 +32,9 @@ var vid;
 var playing = false;
 var completion;
 var introEnd = false;
+var radialArcs = [];
+var soundSpectrum;
+
 //=========================================================================
 // SETUP CODE
 //=========================================================================
@@ -61,10 +64,14 @@ function setup() {
   
 
   angleMode(DEGREES);
+  colorMode(HSB,360,100,100)
   //createBackground();
 
   createButtons();
   createGlobalInputs(); // Create soundFile globally
+
+  initRadialArcs();
+  initSound();
 
   
 
@@ -150,7 +157,7 @@ function createBackground(){
   noStroke();
   text('BLACK LIVES MATTER', 50, 50);
   textSize(30)
-  text(protesters.length + ' VOICES, ADD YOURS', 700, windowHeight - 31);
+  text(protesters.length + ' VOICES, ADD YOURS', (windowWidth/2)-100, windowHeight - 31);
   //text('TAKE ACTION', 200, windowHeight - 50);
 
 
@@ -190,6 +197,131 @@ function playAudio() {
 //}
 
 //=========================================================================
+// RADIAL ARCS
+//=========================================================================
+
+
+function initRadialArcs() {
+  // pass settings into constructor (arcs,minRadius,maxRadius,baseline angle,maxStrokeWidth,minHue,maxHue)
+  radialArcs[0] = new RadialArcs(40, height/2, width, 0, 1, 40, 50); // bass
+  //radialArcs[1] = new RadialArcs(30, height/12, width, 1, 1, 255, 60); // treb
+}
+
+function updateRadialArcs() {
+  radialArcs[0].updateArcs(getNewSoundDataValue("bass")); // bass
+  //radialArcs[1].updateArcs(getNewSoundDataValue("bass")); // treb
+}
+
+function drawRadialArcs() {
+  radialArcs[0].drawArcs(); // bass
+  //radialArcs[1].drawArcs(); // treb
+}
+
+class RadialArcs { // -------------------------   RadialArcs Class -------------------------------
+  constructor(arcCount, minR, maxR, baseR, maxStr, minH, maxH) {
+    this.radialArcCount = arcCount;
+    this.minRadius = minR;
+    this.maxRadius = maxR;
+    this.radialArcs = [];
+    this.baselineR = baseR;
+    this.maxStroke = maxStr;
+    this.minHue = minH;
+    this.maxHue = maxH;
+    this.initArcs();
+  }
+
+  initArcs() {
+    for(let a=0; a<this.radialArcCount; a++) { // create a new radialArc object x radialArcCount
+      // pass vals into constructor (id,arcs,minRadius,maxRadius,cX, cY, baseline angle)
+      this.radialArcs[a] = new RadialArc(a, this.radialArcCount, this.minRadius, this.maxRadius, this.baselineR, this.maxStroke, this.minHue, this.maxHue);   
+    }
+  }
+
+  updateArcs(d)  {
+    for(let a=this.radialArcs.length-1; a >= 0; a--) { // work backwards down array of arcs, 
+      if(a>0) {
+        this.radialArcs[a].setValue(this.radialArcs[a-1].getValue()); // taking value from arc in position ahead in array, so shifting values up the array of arcs by one.
+      } else {
+        this.radialArcs[a].setValue(d); // until last arc, update with new value from data
+      }
+    }
+  }
+
+  drawArcs()  {
+    for(let a=0; a<this.radialArcs.length; a++) {  // loop through array of arcs calling "draw"
+      this.radialArcs[a].redrawFromData();
+    }
+  }
+}
+
+class RadialArc { // -------------------------   RadialArc Class -------------------------------
+  constructor(id, arcs, minR, maxR, baseR, maxStr, minH, maxH) {
+    this.arcID = id;
+    this.totalArcs = arcs;
+    this.minRadius = minR; // min size of arc
+    this.maxRadius = maxR; // max size of arc
+    this.arcRadius = this.minRadius + (((this.maxRadius-this.minRadius)/this.totalArcs)*this.arcID+1); // size of THIS arc based on position in arcs
+    this.maxStroke = maxStr;
+    this.minHue = minH;
+    this.maxHue = maxH;
+    this.dataVal = 0;
+    this.centerX = (windowWidth/2)-50; 
+    this.centerY = windowHeight/2;
+    this.arcMaxRadian = 20; // max length of arc around circumference
+    this.arcBaseline = baseR;
+    this.arcStartRadian = 0; // starting radian of arc
+    this.arcEndRadian = 0; // end radian of this arc (based on data)
+  }
+
+  setValue(d) {
+    this.dataVal = d;
+  }
+
+  getValue() {
+    return this.dataVal;
+  }
+
+  redrawFromData() {
+    this.updateArc();
+    this.drawArc(); 
+  }
+
+  updateArc() {
+    this.arcEndRadian = this.arcBaseline + (this.arcMaxRadian * this.dataVal); // start of arc (radians) based on data
+    this.arcStartRadian = this.arcBaseline - (this.arcMaxRadian * this.dataVal); // end of arc (radians) based on data
+  }
+
+  drawArc() {
+    this.dataColor = this.getDataHSBColor(this.dataVal); // get data scaled colour
+    stroke(this.dataColor); // set stroke colour
+    strokeWeight(map(this.dataVal,0,1,0,this.maxStroke)); // set stroke weight based on data
+    noFill(); // no fill in arc shape
+    arc(this.centerX,this.centerY,this.arcRadius,this.arcRadius,this.arcStartRadian,this.arcEndRadian); // draw arc 
+    arc(this.centerX,this.centerY,this.arcRadius,this.arcRadius,this.arcStartRadian-PI,this.arcEndRadian-PI); // draw reflected arc
+  }
+
+  getDataHSBColor(d) {
+    this.dataHue = map(d,0,1,this.minHue,this.maxHue); // value moves across inout hue range
+    this.dataSaturation = map(d,0,1,100,80); // higher value = lower saturation (more white, when combined with brightness)
+    this.dataBrightness = map(d,0,1,10,100); // higher value = higher brightness (more white, when combined with saturation)
+    return color(this.dataHue,this.dataSaturation,this.dataBrightness);
+  }
+}
+
+function getNewSoundDataValue(freqType) {
+  return map(fft.getEnergy(freqType),0,255,0,2); // get energy from frequency, scaled from 0 to 1
+}
+
+function initSound() {
+  fft = new p5.FFT(0.4,1024); // (smoothing, bins)
+  //soundFile.amp(0.7); 
+}
+
+function analyseSound() {
+  soundSpectrum = fft.analyze(); // spectrum is array of amplitudes of each frequency
+}
+
+//=========================================================================
 // PROTESTER CLASS
 //=========================================================================
   
@@ -217,7 +349,7 @@ class Protester {
 
   display(mouseX, mouseY) {
     noFill();
-    rect(this.x, this.y-29, 50, 30);
+    rect(this.x, this.y-30, 80, 30);
     fill('#F6C516');
     textFont(this.font);
     textSize(30);
@@ -310,20 +442,18 @@ function draw() {
   
   completion = vid.time() / vid.duration();
   if (completion == 1) {
-  vid.hide();
-  createBackground(); // main bg
-   
+  vid.hide(); // main bg
+  createBackground(); 
+  analyseSound();
+  updateRadialArcs();
+  drawRadialArcs();
 }
-
 
 //PROTESTERS MOVING
   for (let i = 0; i < protesters.length; i++) {
     protesters[i].move();
     protesters[i].display(mouseX, mouseY);
   }
-
-
-
 
 //=============WAVEFORM ==========
 /*
@@ -386,20 +516,10 @@ function draw() {
   
 }
 
-
-
-
-
-///PLAY/PAUSE VIDEO - not needed, now loading on load
-
 function mousePressed() {
   if (!playing) {
     vid.play();
     vid.time((mouseX/width) * vid.duration());
     playing = true;
   }
-  //else {
-    //vid.pause();
-   // playing = false;
-  //}
 }
